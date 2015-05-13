119985d32eddd005f366e6995353c5651d6b9ad2
mizan7780 <mizan7780@gmail.com>
2015-05-13 13:22:17 +0600

    ArrayList

---
+++ ArrayListIntroApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListIntroApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+
+            al.Add(14);
+            al.Add("SEIP");
+            al.Add(16.8);
+
+            al.Reverse();
+            //al.Sort(); declare same dataType to sort ArrayList
+
+            foreach (var a in al)
+            {
+                Console.WriteLine(a);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
